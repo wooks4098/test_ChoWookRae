c28b9b2d4a3d437f7054628a0ef3020c96dec0fb
dlathwjd2428 <69287243+dlathwjd2428@users.noreply.github.com>
2021-01-13 20:50:22 +0900

    winapi

--- WinAPI/WinMain/WinMain.cpp
+++ WinAPI/WinMain/WinMain.cpp
@@ -1,5 +1,5 @@
 #include<windows.h>
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM); 
 HINSTANCE g_hInst;//글로벌 인스턴스핸들값
 LPCTSTR lpszClass = TEXT("HelloWorld"); //창이름
 
@@ -7,30 +7,46 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmd
 {
 	HWND hWnd;
 	MSG Message;
-	WNDCLASS WndClass;
-	g_hInst = hInstance;
-	//WndClass는 기본 윈도우환경을 만드는 구조체다. 맴버변수는 밑에와 같다.
-	WndClass.cbClsExtra = 0; //예약영역
-	WndClass.cbWndExtra = 0; //예약영역 (신경x)
-	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	//배경색
-	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);	//커서
-	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);	//아이콘 모양
-	WndClass.hInstance = hInstance;//(프로그램 핸들값(번호)등록)
-	WndClass.lpfnWndProc = WndProc;	//프로세스 함수 호출
-	WndClass.lpszClassName = lpszClass;	//클레스 이름
-	WndClass.lpszMenuName = NULL;
-	WndClass.style = CS_HREDRAW | CS_VREDRAW;//윈도우의 수직과 수평이 변경 시 다시 그린다.
-	RegisterClass(&WndClass);  //만들어진 WidClass를 등록
 
-	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
-	ShowWindow(hWnd, nCmdShow);
+	//1. 윈도우 클래스 정의
+		WNDCLASS WndClass; 
+		g_hInst = hInstance;
+		//WndClass는 기본 윈도우환경을 만드는 구조체다. 맴버변수는 밑에와 같다.
+		WndClass.cbClsExtra = 0; //예약영역 (신경x)
+		WndClass.cbWndExtra = 0; //예약영역 (신경x)
+		WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //배경색
+		WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);	//커서
+		WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION); //아이콘 모양
+		WndClass.hInstance = hInstance;//(프로그램 핸들값(번호)등록)
+		WndClass.lpfnWndProc = WndProc;	//프로세스 함수 호출
+		WndClass.lpszClassName = lpszClass;	//클레스 이름
+		WndClass.lpszMenuName = NULL;
+		WndClass.style = CS_HREDRAW | CS_VREDRAW;//윈도우의 수직과 수평이 변경 시 다시 그린다. (확장 가능 불가능 정하기)
 
-	while (GetMessage(&Message, NULL, 0, 0))//사용자에게 메시지를 받아오는 함수(WM_QUIT 메시지 받을 시 종료)
-	{
-		TranslateMessage(&Message); //  키보드 입력 메시지 처리함수
-		DispatchMessage(&Message); //받은 메시지를 WndProc에 전달하는 함수
-	}
-	return (int)Message.wParam;
+	//2. 윈도우 클래스 등록
+		RegisterClass(&WndClass);  //만들어진 WidClass를 등록
+
+
+	//3. 윈도우 생성
+		hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+
+
+	//4. 윈도우를 화면에 표시
+		ShowWindow(hWnd, nCmdShow); //윈도우를 화면에 표시
+		
+
+
+	//5. 메시지를 받아 처리
+		while (GetMessage(&Message, NULL, 0, 0))//사용자에게 메시지를 받아오는 함수(WM_QUIT 메시지 받을 시 종료)
+		{
+			TranslateMessage(&Message); //  키보드 입력 메시지 처리함수
+			DispatchMessage(&Message); //받은 메시지를 WndProc에 전달하는 함수
+		}
+
+
+
+		return (int)Message.wParam;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
