037cba8ea9f937522890053d98a9ec21a1fa7d1e
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-03-02 17:01:25 +0900

    Chess

--- 과제/WinAPI/9_체스/WinMain.cpp
+++ 과제/WinAPI/9_체스/WinMain.cpp
@@ -37,45 +37,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmd
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
-	HDC hdc;
-	PAINTSTRUCT ps;
-	HDC hMemDC;
-	HBITMAP hImage, hOldBitmap;
-	int bx, by;
-	BITMAP bit;
 	switch (iMessage)
 	{
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		hMemDC = CreateCompatibleDC(hdc);
-
-		hImage = (HBITMAP)LoadImage(NULL, TEXT("image.bmp"),
-			IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
-
-		hOldBitmap = (HBITMAP)SelectObject(hMemDC, hImage);
-
-		GetObject(hImage, sizeof(BITMAP), &bit);
-		bx = bit.bmWidth;
-		by = bit.bmHeight;
-
-		//원본 그대로
-		//화면의 (50, 50) 위치에 원본 크기로 원본의 (0, 0) 위치부터 추출하여 그린다
-		BitBlt(hdc, 50, 50, bx, by, hMemDC, 0, 0, SRCCOPY);
-
-		//투명처리
-		//RGB(255, 0, 255)(핑크색)을 투명하게 만들고 그린다
-		TransparentBlt(hdc, 350, 50, bx, by, hMemDC, 0, 0, bx, by, RGB(255, 0, 255));
-
-		//투명처리와 부분그리기
-		//이번에는 왼쪽을 보고 있고 한발을 내딛은 캐릭터를 그린다
-		TransparentBlt(hdc, 650, 50, bx / 4, by / 4,
-			hMemDC, (bx / 4) * 1, (by / 4) * 2, bx / 4, by / 4, RGB(255, 0, 255));
-
-		SelectObject(hMemDC, hOldBitmap);
-		DeleteObject(hImage);
-		DeleteDC(hMemDC);
-		EndPaint(hWnd, &ps);
-		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
